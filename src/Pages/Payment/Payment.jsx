a5919c6cd1fea8cf1fc08805f556308be7258205
himanshu1856 <hs287190@gmail.com>
2023-04-07 17:30:42 +0530

    payment page is finished except payment nav

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,195 +1,292 @@
 import React from 'react'
-import './payment.css'
+import { useState } from 'react'
 
 import frickly from '../../assets/icons/frikly.webp'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGreaterThan, faLock, faCartShopping, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import './style/payment.css'
+import './style/paymentHeader.css'
+import './style/personalDetails.css'
+import './style/shippingDetails.css'
+import Lock from '../../assets/icons/Lock.webp'
+import Cart from '../../assets/icons/cart.webp'
+import CartSummary from '../../assets/icons/cart-summart.png'
 
-import cartPhoto from '../../assets/icons/cart-summart.png'
 
-let PersonalDetails = () => {
+let PaymentHeader = () => {
   return (
     <>
-      <div className="row">
-
-        <div className="col offset-2 ps-4" style={{ textAlign: "start" }}>
-          <h4>Personal Details</h4>
-          <div className="row mt-4">
-            <label htmlFor="name">Name</label>
-            <input class="form-control ms-2" type="search" name='name' ></input>
-          </div>
-          <div className="row mt-3">
-            <label htmlFor="phone">Phone Number</label>
-            <input class="form-control ms-2" type="search" name='phone' ></input>
-          </div>
-          <div className="row mt-3">
-            <label htmlFor="whatsapp">Whatsapp Number</label>
-            <input class="form-control ms-2" type="search" name='whatsapp' ></input>
-          </div>
-          <div className="row mt-3">
-            <div className="col-1 me-md-2 me-lg-0 fs-md-[20px]">
-              <input class="form-check-input" type="checkbox" />
-            </div>
-            <div className="col p-0" style={{ textAlign: "start", fontWeight: "300" }}>
-              Whatsapp number Same as phone number
-            </div>
-          </div>
-          <button className='btn mt-5 mb-5 p-2' style={{
-            backgroundColor: "#EAE0D5"
-          }}>Continue &nbsp; <FontAwesomeIcon icon={faArrowRight} /> &nbsp;</button>
+      <div className="payment-header">
+        <div className="frickly-logo">
+          <img src={frickly} alt="frickly" />
+        </div>
+        <div className="payment-nav">
+          <p>Payment Details > Shopping Details > Payment Info</p>
+        </div>
+        <div className="secure-login">
+          <p> <img src={Lock} alt="lock" /> <span className="secure-login-text">100% secure login</span> </p>
+        </div>
+        <div className="back-to-cart">
+          <p> <img src={Cart} alt="cart" /> <span className="back-to-cart-text">Back to Cart</span> </p>
         </div>
       </div>
-
     </>
   )
 }
 
-let ShippingDetails = () => {
+let PersonalDetails = (props) => {
   return (
-    <div className='row'>
-      <div className="col-6 offset-1 ps-4 mt-3" style={{ textAlign: "start" }}>
-        <h4>Shipping Details</h4>
-        <div className="row mt-4">
-          <label htmlFor="address1">Address1</label>
-          <input class="form-control ms-2" type="search" name='address1' ></input>
-        </div>
-        <div className="row mt-3">
-          <label htmlFor="address12">Address2</label>
-          <input class="form-control ms-2" type="search" name='address2' ></input>
-        </div>
-        <div className="row mt-3">
-          <div className="col">
-            <label htmlFor="city">City</label>
-            <input class="form-control " type="search" name='city' ></input>
+    <>
+      <div className="personal-details">
+        <h3>Personal Details</h3>
+        <div className="personal-details-container">
+          <div>
+            <label htmlFor="personal-name">Name</label>
+            <input class="form-control me-2" type="search" name='personal-name'></input>
           </div>
-          <div className="col">
-            <label htmlFor="pincode">Pincode</label>
-            <input class="form-control " type="search" name='pincode' ></input>
+          <div>
+            <label htmlFor="phone-number">Phone Number</label>
+            <input class="form-control me-2" type="search" name='phone-number'></input>
           </div>
-        </div>
-        <div className="row mt-3">
-          <label htmlFor="state">State</label>
-          <input class="form-control ms-2" type="search" name='state' ></input>
-        </div>
-        <div className="row mt-3">
-          <label htmlFor="gst">GST Number</label>
-          <input class="form-control ms-2" type="search" name='gst' ></input>
-        </div>
-        <div className="row mt-3">
-          <div className="col-1">
-            <input class="form-check-input" type="checkbox" />
+          <div>
+            <label htmlFor="whatsapp-number">Whatsapp Number</label>
+            <input class="form-control me-2" type="search" name='whatsapp-number'></input>
           </div>
-          <div className="col p-0" style={{ textAlign: "start", fontWeight: "300" }}>
-            Billing address same as shipping address
+          <div className="wtsp-same-as">
+            <div>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>
+              <p>Whatsapp number Same as phone number</p>
+            </div>
           </div>
-        </div>
-        <div className="row mt-3">
-          <div className="col">
-            <button className='btn mt-5 mb-5 p-2' style={{
-              // backgroundColor: "#EAE0D5"
-            }}> <FontAwesomeIcon icon={faArrowLeft} /> &nbsp; Previous &nbsp;</button>
-          </div>
-          <div className="col" style={{ textAlign: "right" }}>
-            <button className='btn mt-5  mb-5 p-2' style={{
-              backgroundColor: "#EAE0D5"
-            }}>Continue &nbsp; <FontAwesomeIcon icon={faArrowRight} /> &nbsp;</button>
-          </div>
+          <button className='btn continue-button'
+            onClick={
+              () => {
+                props.togglePersonalDetails()
+              }
+            }
+          >Continue</button>
         </div>
       </div>
-    </div>
+    </>
+  )
+}
+
+let PersonalDetailsReceit = () => {
+  return (
+    <>
+      <div className="personal-details-receit">
+        <div>
+          <h3>Personal Details</h3>
+        </div>
+        <div style={{ textDecoration: "underline" }}>
+          Change
+        </div>
+      </div>
+      <p style={{ textAlign: "left", margin: "0" }}>Arpit Chandak</p>
+      <p style={{ textAlign: "left" }}>+91 8017555192</p>
+    </>
+  )
+}
+
+let ShippingDetails = (props) => {
+  return (
+    <>
+      <hr />
+      <div className="shipping-details">
+        <h3>Shipping Details</h3>
+
+        <div className="shipping-details-container">
+          <div>
+            <label htmlFor="address1">Address 1</label>
+            <input class="form-control me-2" type="search" name='address1'></input>
+          </div>
+          <div>
+            <label htmlFor="address2">Address 2</label>
+            <input class="form-control me-2" type="search" name='address2'></input>
+          </div>
+          <div className="city-pincode">
+            <div>
+              <label htmlFor="city">City</label>
+              <input class="form-control me-2" type="search" name='city'></input>
+            </div>
+            <div>
+              <label htmlFor="pincode">Pincode</label>
+              <input class="form-control me-2" type="search" name='pincode'></input>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="state">State</label>
+            <input class="form-control me-2" type="search" name='state'></input>
+          </div>
+          <div>
+            <label htmlFor="gst">GST Number</label>
+            <input class="form-control me-2" type="search" name='gst'></input>
+          </div>
+          <div className="wtsp-same-as">
+            <div>
+              <input type="checkbox" />
+            </div>
+            <div>
+              <p>Billing address same as shipping address </p>
+            </div>
+          </div>
+        </div>
+
+        <h3 style={{ marginTop: "50px" }}>Billing Details</h3>
+
+        <div className="shipping-details-container">
+          <div>
+            <label htmlFor="address1">Address 1</label>
+            <input class="form-control me-2" type="search" name='address1'></input>
+          </div>
+          <div>
+            <label htmlFor="address2">Address 2</label>
+            <input class="form-control me-2" type="search" name='address2'></input>
+          </div>
+          <div className="city-pincode">
+            <div>
+              <label htmlFor="city">City</label>
+              <input class="form-control me-2" type="search" name='city'></input>
+            </div>
+            <div>
+              <label htmlFor="pincode">Pincode</label>
+              <input class="form-control me-2" type="search" name='pincode'></input>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="state">State</label>
+            <input class="form-control me-2" type="search" name='state'></input>
+          </div>
+
+        </div>
+
+        <div className="navigation-buttons mt-3">
+          <div>
+            <button className='btn p-0'>Previous</button>
+          </div>
+          <div>
+            <button className='btn ' style={{ backgroundColor: "#EAE0D5" }} onClick={() => { props.togggleShippingDetails() }}>Continue</button>
+          </div>
+        </div>
+
+
+      </div>
+    </>
+  )
+}
+
+let ShippingDetailsReceit = () => {
+  return (
+    <>
+      <hr />
+      <div className="personal-details-receit">
+        <div>
+          <h3>Shipping Details</h3>
+        </div>
+        <div style={{ textDecoration: "underline" }}>
+          Change
+        </div>
+      </div>
+      <p style={{ textAlign: "left", margin: "0" }}>Arpit Chandak</p>
+      <p style={{ textAlign: "left" }}>+91 8017555192</p>
+    </>
   )
 }
 
 let PaymentInfo = () => {
   return (
     <>
-      <div className='row ps-4'>
-        <div className="col-4 offset-1" style={{ textAlign: "start" }}>
-          <h4>Payment Info</h4>
-          <div className="row mt-3 ms-1" style={{ border: "1px solid black",borderRadius:"10px",padding:"10px"}}>
-            <div className="col-2">
-              <input class="form-check-input mt-0 fs-4" type="radio" />
-            </div>
-            <div className="col ">
-              <div className="row fw-bold">UPI, Debit Card or Net Banking</div>
-            </div>
+      <hr />
+      <div className="payment-info">
+        <h3>Payment Info</h3>
+        <div style={{ border: "1px solid black", borderRadius: "5px", padding: "10px", display: "flex", alignItems: "center" }}>
+          <div>
+            <input type="radio" name='upi' style={{ marginRight: "10px", height: "20px", width: "20px" }} />
           </div>
-          <div className="row mt-3 ms-1" style={{ border: "1px solid black" ,borderRadius:"10px",padding:"10px"}}>
-            <div className="col-2">
-              <input class="form-check-input mt-0 fs-4" type="radio"  />
-            </div>
-            <div className="col">
-              <div className="row fw-bold">Pay On Delivery</div>
-              <div className="row mt-2"> <p> 5% of total order value is charged upfront via online payment. <span className='fw-light p-0 text-decoration-underline'>Why do we charge 5%? </span></p> </div>
-            </div>
-          </div>
-          <button className='btn mt-5' style={{backgroundColor:"#22333B",color:"white",padding:"10px",fontSize:"14px",width:"50%"}}>Preview & Place your Order</button>
+          <p style={{ display: "inline-block", margin: "0", fontWeight: "500" }} name="upi">UPI, Debit Card or Net Banking</p>
         </div>
-      </div>
-    </>
-  )
-}
-
-let CartSummary = () => {
-  return (
-    <>
-      <img src={cartPhoto} alt="cart" style={{ height: "337px", width: "367px" }} />
-    </>
-  )
-}
-
-let PayMentHeader = () => {
-  return (
-    <>
-      <div className="container-fluid payment-header">
-        <div className="row" >
-          <div className="col-1 offset-1 ps-4" style={{ textAlign: "start", paddingTop: "30px" }}>
-            <Link to="/home"><img src={frickly} alt="frickly" style={{ height: "40px" }} /></Link>
+        <div style={{ border: "1px solid black", borderRadius: "5px", padding: "10px", display: "flex", alignItems: "top", marginTop: "20px" }}>
+          <div>
+            <input type="radio" name='upi' style={{ marginRight: "10px", height: "20px", width: "20px"}} />
           </div>
-          <div className="col-4 me-5" style={{ textAlign: "center", paddingTop: "43px" }}>
-            <p>Personal Details <FontAwesomeIcon icon={faGreaterThan} className='ms-1 me-2' /> Shopping Details <FontAwesomeIcon icon={faGreaterThan} className='ms-1 me-2' />Payment info </p>
-          </div>
-          <div className="col-2 offset-1" style={{ textAlign: "center", paddingTop: "43px" }}>
-            <p><FontAwesomeIcon icon={faLock} className='me-1' /> 100% Secure Login</p>
-          </div>
-          <div className="col-2" style={{ textAlign: "start", paddingTop: "43px" }}>
-            <p><FontAwesomeIcon icon={faCartShopping} className='me-2' /><span style={{ textDecoration: "underline" }}>Back to Cart</span> </p>
+          <div style={{ display: "inline-block", margin: "0" }} name="upi">
+            <p style={{ fontWeight: "500" }}>Pay On Delivery</p>
+            <p style={{ fontSize: "12px" }}>5% of total order value is charged upfront via online payment. <span style={{ textDecoration: "underline" }}> Why do we charge 5%?</span> </p>
           </div>
         </div>
+
       </div>
+      <buttton className="btn place-order m-3" style={{backgroundColor:"#22333B", color:"white"}}>
+        Preview & place your order
+      </buttton>
     </>
   )
 }
-
 
 function Payment(props) {
+
+  const [personalDetails, setPersonalDetails] = useState(true)
+  const [shippingDetails, setShippingDetails] = useState(true)
+
+  let togglePersonalDetails = () => {
+    if (personalDetails) {
+      setPersonalDetails(false)
+    }
+    else {
+      setPersonalDetails(true)
+    }
+  }
+
+  let togggleShippingDetails = () => {
+    if (shippingDetails) {
+      setShippingDetails(false)
+    }
+    else {
+      setShippingDetails(true)
+    }
+  }
+
   props.paymentOpen()
   return (
-    <div style={{ height: "2000px" }}>
-      <PayMentHeader />
+    <>
+      <div className="payment">
+        <PaymentHeader state={{personalDetails:personalDetails, shippingDetails:shippingDetails}}/>
+        <div className="payment-container">
+          <div className="payment-data">
+            {
+              (personalDetails)
+                ? (< PersonalDetails togglePersonalDetails={togglePersonalDetails} />)
+                : (<PersonalDetailsReceit />)
+            }
+            {
+              (shippingDetails && !personalDetails)
+                ? (<ShippingDetails togggleShippingDetails={togggleShippingDetails} />)
+                : (
+                  (personalDetails)
+                    ? (<>
+                      <hr />
+                      <h3 style={{ textAlign: "left", marginTop: "20px" }}>Shipping Details</h3>
+                    </>
+                    )
+                    : (<ShippingDetailsReceit />)
+                )
+            }
 
-      <div className="container-fluid">
-
-        <div className="row mt-5">
-          <div className="col">
-            <PersonalDetails />
+            {
+              (!personalDetails && !shippingDetails)
+                ? (<PaymentInfo />)
+                : (<>
+                  <hr />
+                  <h3 style={{ textAlign: "left", marginTop: "20px" }}>Payment Info</h3>
+                </>)
+            }
           </div>
-
-          <div className="col ps-5">
-            <CartSummary />
+          <div className="cart-info">
+            <img src={CartSummary} alt="Cart Summry" />
           </div>
-
-
         </div>
       </div>
-
-      <hr style={{ width: "82%", marginLeft: "100px" }} />
-      <ShippingDetails />
-      <hr style={{ width: "82%", marginLeft: "100px" }} />
-      <PaymentInfo />
-
-    </div>
+    </>
   )
 }
 
--- src/Pages/Product/SingleProduct.jsx
+++ src/Pages/Product/SingleProduct.jsx
@@ -8,64 +8,66 @@ import sofa2 from '../../assets/icons/sofa2.webp'
 import sofa1 from '../../assets/icons/sofa1.webp'
 
 let ProductCard = () => {
-  
+
   const [img1, setImg1] = useState(sofa1)
   const [img2, setImg2] = useState(sofa2)
   const [img3, setImg3] = useState(sofa3)
 
   let swapImages = (index) => {
-      if (index == 1) {
-          return
-      }
-      else if (index == 2) {
-          let temp = img1;
-          setImg1(img2);
-          setImg2(temp);
-      }
-      else if (index == 3) {
-          let temp = img1;
-          setImg1(img3);
-          setImg3(temp);
-      }
+    if (index == 1) {
+      return
+    }
+    else if (index == 2) {
+      let temp = img1;
+      setImg1(img2);
+      setImg2(temp);
+    }
+    else if (index == 3) {
+      let temp = img1;
+      setImg1(img3);
+      setImg3(temp);
+    }
   }
 
   return (
     <>
-        <div className="product-card-container">
-            <div className="product-card-main-image" onClick={()=>{swapImages(1)}}>
-                <img src={img1} alt="Product" />
-            </div>
-            <div className="product-card-option-image">
-                <div onClick={()=>{swapImages(2)}}>
-                    <img src={img2} alt="Product"  />
-                </div>
-                <div onClick={()=>{swapImages(3)}}>
-                    <img src={img3} alt="Product"  />
-                </div>
-            </div>
-            <p className="card-title">Ainsley Rolled Arm Sofa
-                74.01” by Steelside</p>
-            <div className="card-pricing">
-                <p className='mrp' style={{ color: "#919191" }}>MRP : <faIndianRupeeSign /> <span style={{ textDecoration: "line-through" }}>17,999</span> <span style={{ backgroundColor: "black", color: "white", padding: "2px" }}>11% off</span> </p>
-                <p><span className='card-current-price'><faIndianRupeeSign /> 15,999</span> <span style={{ fontSize: "13px", fontWeight: "400" }}>/ piece</span></p>
-            </div>
-      
+      <div className="product-card-container">
+        <div className="product-card-main-image" onClick={() => { swapImages(1) }}>
+          <img src={img1} alt="Product" />
         </div>
+        <div className="product-card-option-image">
+          <div onClick={() => { swapImages(2) }}>
+            <img src={img2} alt="Product" />
+          </div>
+          <div onClick={() => { swapImages(3) }}>
+            <img src={img3} alt="Product" />
+          </div>
+        </div>
+        <p className="card-title">Ainsley Rolled Arm Sofa
+          74.01” by Steelside</p>
+        <div className="card-pricing">
+          <p className='mrp' style={{ color: "#919191" }}>MRP : <faIndianRupeeSign /> <span style={{ textDecoration: "line-through" }}>17,999</span> <span style={{ backgroundColor: "black", color: "white", padding: "2px" }}>11% off</span> </p>
+          <p><span className='card-current-price'><faIndianRupeeSign /> 15,999</span> <span style={{ fontSize: "13px", fontWeight: "400" }}>/ piece</span></p>
+        </div>
+        <div style={{display:"flex",width:"100%"}}>
+          <button className='btn' style={{ backgroundColor: "lightgray" }}> Add to Cart</button>
+        </div>
+      </div>
     </>
-)
+  )
 
 }
 
 let SimilarProduct = () => {
-  return(
+  return (
     <>
       <div className="similar-products">
         <h2>Similar Products</h2>
         <div className="similar-products-container">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
         </div>
       </div>
     </>
@@ -73,15 +75,15 @@ let SimilarProduct = () => {
 }
 
 let RecentlyViewed = () => {
-  return(
+  return (
     <>
       <div className="similar-products">
         <h2>Recently Viewed</h2>
         <div className="similar-products-container">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
         </div>
       </div>
     </>
@@ -94,8 +96,8 @@ function SingleProduct() {
 
       <p></p>
 
-      <SimilarProduct/>
-      <RecentlyViewed/>
+      <SimilarProduct />
+      <RecentlyViewed />
     </div>
   )
 }
