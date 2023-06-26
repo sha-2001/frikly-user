97be66430a55e1a9628fe730d87fcc013a9752cf
sha-2001 <himanshs@frikly.com>
2023-06-26 10:36:14 +0530

    header rewamping

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,36 +3,13 @@ import React from 'react'
 import './home.css';
 
 import DesignedForYou from './DesignedForYou';
-import FriklyWhatsapp from './FriklyWhatsApp';
-import ReasonsToBuy from './ReasonsToBuy';
-import OnTheWay from './OnTheWay';
-import Deal from './Deal';
 import ProMember from './ProMember';
-import Testimonial from './Testimonial';
-import BuyingInBulk from './BuyingInBulk';
-import SellOnFrikly from './SellOnFrikly';
-import HeroBanner from './HeroBanner';
 
 function Home(props) {
   props.paymentClose()
   return (
     <div>
-      {/* <HeroBanner/> */}
-      {/* <Deal />
-      <div style={{
-        height: '100vh',
-        margin: '50px',
-        backgroundColor: 'gray',
-        borderRadius: '20px',
-      }}></div> */}
-
-      <OnTheWay />
-      {/* <ReasonsToBuy /> */}
-      {/* <SellOnFrikly /> */}
-      {/* <FriklyWhatsapp /> */}
       <ProMember />
-      {/* <Testimonial /> */}
-      {/* <BuyingInBulk /> */}
       <DesignedForYou />
     </div>
   )
